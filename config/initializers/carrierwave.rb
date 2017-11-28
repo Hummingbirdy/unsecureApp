a9307acf591ac5b250760a6a25d815e2ff890b91
tlemaster <taylorlemaster@my.unt.edu>
2017-11-28 16:47:58 -0600

    fix buttons on show page

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,13 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
-      region:                ENV['S3_REGION'],
+      # aws_access_key_id:     ENV['S3_KEY'],
+      # aws_secret_access_key: ENV['S3_SECRET'],
+      # region:                ENV['S3_REGION'],
+      aws_access_key_id:     'AKIAIEDU2SFQANK2533Q',
+      aws_secret_access_key: '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',
+      region:                'us-east-2',
   }
-  config.fog_directory  = ENV['S3_BUCKET']
+ # config.fog_directory  = ENV['S3_BUCKET']
+  config.fog_directory  = 'myrecipes-tlemaster'
 end
